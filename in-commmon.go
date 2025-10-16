63c1fbfe2f1eed393f67ef0a4dc42cdbe69be8f7
Dave Palica <30877122+dwp32@users.noreply.github.com>
2025-10-16 15:08:50 -0600

    Removed exteraneous api endpoints and code.  Got eduPerson endpoint working with basic informaiton.

--- in-commmon.go
+++ in-commmon.go
@@ -75,14 +75,14 @@ type APICreds struct {
 }
 
 type Person struct {
-	netId              string   `json:"netId"`
-	netIdScoped        string   `json:"netIdScoped"`
-	emailAddress       string   `json:"emailAddress"`
-	primaryAffiliation string   `json:"eduPersonPrimaryAffiliation"`
-	affiliations       []string `json:"eduPersonAffiliation"`
-	scopedAffiliations []string `json:"eduPersonScopedAffiliation"`
-	name               string   `json:"name"`
-	preferredFirstName string   `json:"preferredFirstName"`
+	NetID              string   `json:"netId"`
+	NetIDScoped        string   `json:"netIdScoped"`
+	EmailAddress       string   `json:"emailAddress"`
+	PrimaryAffiliation string   `json:"primaryAffiliation"`
+	Affiliations       []string `json:"affiliations"`
+	ScopedAffiliations []string `json:"scopedAffiliations"`
+	Name               string   `json:"name"`
+	PreferredFirstName string   `json:"preferredFirstName"`
 }
 
 // This is the structure that contains all of the information. All or parts may be returned, depend
@@ -218,8 +218,8 @@ func getUserBasicInfo(netId string) (*Person, error) {
 			value, ok := netID["value"].(string)
 			if ok {
 				fmt.Println("NetID:", value)
-				person.netId = value
-				person.netIdScoped = value + "@byu.edu"
+				person.NetID = value
+				person.NetIDScoped = value + "@byu.edu"
 			}
 		}
 	}
@@ -228,6 +228,7 @@ func getUserBasicInfo(netId string) (*Person, error) {
 		return nil, err
 	}
 	//	person.netId =
+	fmt.Println(person)
 	return &person, nil
 }
 
@@ -247,7 +248,7 @@ func main() {
 
 	// Renew the global token as needed. Here we set it to renew every 10 seconds for testing purposes. In production, set it to 3500 seconds (just before the 1 hour expiration).
 	// The timer runs in a separate goroutine.
-	startTimer(10 * time.Second)
+	startTimer(3500 * time.Second)
 
 	if err != nil {
 		log.Fatalf("Failed to get token: %v", err)
@@ -270,11 +271,7 @@ func main() {
 
 	// Define the routes and their handlers
 
-	gRouter.HandleFunc("/api/eduPersonAffiliation", getEduPersonAffiliation).Methods("GET")
-
-	gRouter.HandleFunc("/api/name", getName).Methods("GET")
-	gRouter.HandleFunc("/api/preferredFirstName", getPreferredFirstName).Methods("GET")
-	gRouter.HandleFunc("/api/eduPersonAll", getAll).Methods("GET")
+	gRouter.HandleFunc("/api/eduPerson", eduPerson).Methods("GET")
 	gRouter.HandleFunc("/api/help", help).Methods("GET")
 
 	// Start the server
@@ -284,196 +281,36 @@ func main() {
 
 // initDB initializes the database connection
 
-func getName(w http.ResponseWriter, r *http.Request) {
-	consoleMsg := "getName called"
-	log.Println(consoleMsg)
+func eduPerson(w http.ResponseWriter, r *http.Request) {
+	log.Println("Test called")
 
-	// Get the netId from the query parameters
-	netId := r.URL.Query().Get("netId")
-	if netId == "" {
-		http.Error(w, "Missing netId parameter", http.StatusBadRequest)
+	// Get the byuId from the query parameters
+	byuId := r.URL.Query().Get("byuId")
+	if byuId == "" {
+		http.Error(w, "Missing byuId parameter", http.StatusBadRequest)
+		return
+	}
+
+	// Fetch user info using the provided byuId
+	person, err := getUserBasicInfo(byuId)
+	if err != nil {
+		log.Printf("Failed to get user info: %v", err)
+		http.Error(w, "Failed to retrieve user info", http.StatusInternalServerError)
 		return
 	}
 
 	// Set the response header to application/json
 	w.Header().Set("Content-Type", "application/json")
-
-	// Return the name as JSON
-	/*	response := map[string]string{"name": name}
-		if err := json.NewEncoder(w).Encode(response); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	*/
-}
-
-func getPreferredFirstName(w http.ResponseWriter, r *http.Request) {
-	consoleMsg := "getPreferredFirstName called"
-	log.Println(consoleMsg)
-
-	// Get the netId from the query parameters
-	netId := r.URL.Query().Get("netId")
-	if netId == "" {
-		http.Error(w, "Missing netId parameter", http.StatusBadRequest)
+	//var persont = `{"netId": "jdoe"}`
+	// Encode struct to JSON and write the response
+	if err := json.NewEncoder(w).Encode(person); err != nil {
+		log.Printf("Failed to encode response: %v", err)
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
 		return
 	}
 
-	// Set the response header to application/json
-	w.Header().Set("Content-Type", "application/json")
-
-	// Return the preferredFirstName as JSON
-	/*	response := map[string]string{"preferredFirstName": preferredFirstName}
-		if err := json.NewEncoder(w).Encode(response); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}*/
-}
-
-// Get eduPersonAffiliation
-// Requires netId as input
-// Returns one or more of the following: faculty, staff, student, alum, member, affiliate, employee, contractor, library-walk-in
-// or returns an error if the netId is not found or if there is a database error
-
-func getEduPersonAffiliation(w http.ResponseWriter, r *http.Request) {
-	consoleMsg := "getEduPersonAffiliation called"
-	log.Println(consoleMsg)
-
-	// Get the netId from the query parameters
-	netId := r.URL.Query().Get("netId")
-	if netId == "" {
-		http.Error(w, "Missing netId parameter", http.StatusBadRequest)
-		return
-	}
-
-	/*	var affiliations []string
-
-		if student {
-			affiliations = append(affiliations, "student")
-		}
-		if faculty {
-			affiliations = append(affiliations, "faculty")
-		}
-		if staff {
-			affiliations = append(affiliations, "staff")
-		}
-		if alum {
-			affiliations = append(affiliations, "alum")
-		}
-		if member {
-			affiliations = append(affiliations, "member")
-		}
-		if affiliate {
-			affiliations = append(affiliations, "affiliate")
-		}
-		if employee {
-			affiliations = append(affiliations, "employee")
-		}
-		if primaryaffiliation == "contractor" {
-			affiliations = append(affiliations, "contractor")
-		}
-		if primaryaffiliation == "library-walk-in" {
-			affiliations = append(affiliations, "library-walk-in")
-		}
-		/*
-			if len(affiliations) == 0 {
-				http.Error(w, "No affiliations found for the given netId", http.StatusNotFound)
-				return
-			}
-	*/
-	// Set the response header to application/json
-	/*	w.Header().Set("Content-Type", "application/json")
-
-		// Return the eduPersonAffiliation as JSON
-		response := map[string][]string{"eduPersonAffiliation": affiliations}
-		if err := json.NewEncoder(w).Encode(response); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}*/
-}
-
-func getAll(w http.ResponseWriter, r *http.Request) {
-
-	type UserInfo struct {
-		Name                        string   `json:"name"`
-		PreferredFirstName          string   `json:"preferredFirstName"`
-		EduPersonPrimaryAffiliation string   `json:"eduPersonPrimaryAffiliation"`
-		EduPersonAffiliation        []string `json:"eduPersonAffiliation"`
-		EduPersonScopedAffiliation  []string `json:"eduPersonScopedAffiliation"`
-	}
-	// Create the user object
-	//user := UserInfo{}
-
-	consoleMsg := "getAll called"
-	log.Println(consoleMsg)
-
-	/*
-	   err := row.Scan(&student, &faculty, &staff, &alum, &member, &affiliate, &employee, &contractor, &library, &primaryAffiliation, &user.Name, &user.PreferredFirstName)
-
-	   	if err != nil {
-	   		if err == sql.ErrNoRows {
-	   			http.Error(w, "User not found", http.StatusNotFound)
-	   		} else {
-	   			http.Error(w, "Database error: "+err.Error(), http.StatusInternalServerError)
-	   		}
-	   		return
-	   	}
-
-	   	if student {
-	   		user.EduPersonAffiliation = append(user.EduPersonAffiliation, "student")
-	   		user.EduPersonScopedAffiliation = append(user.EduPersonScopedAffiliation, "student@byu.edu")
-	   	}
-
-	   	if faculty {
-	   		user.EduPersonAffiliation = append(user.EduPersonAffiliation, "faculty")
-	   		user.EduPersonScopedAffiliation = append(user.EduPersonScopedAffiliation, "faculty@byu.edu")
-	   	}
-
-	   	if staff {
-	   		user.EduPersonAffiliation = append(user.EduPersonAffiliation, "staff")
-	   		user.EduPersonScopedAffiliation = append(user.EduPersonScopedAffiliation, "staff@byu.edu")
-	   	}
-
-	   	if alum {
-	   		user.EduPersonAffiliation = append(user.EduPersonAffiliation, "alum")
-	   		user.EduPersonScopedAffiliation = append(user.EduPersonScopedAffiliation, "alum@byu.edu")
-	   	}
-
-	   	if member {
-	   		user.EduPersonAffiliation = append(user.EduPersonAffiliation, "member")
-	   		user.EduPersonScopedAffiliation = append(user.EduPersonScopedAffiliation, "member@byu.edu")
-	   	}
-
-	   	if affiliate {
-	   		user.EduPersonAffiliation = append(user.EduPersonAffiliation, "affiliate")
-	   		user.EduPersonScopedAffiliation = append(user.EduPersonScopedAffiliation, "affiliate@byu.edu")
-	   	}
-
-	   	if employee {
-	   		user.EduPersonAffiliation = append(user.EduPersonAffiliation, "employee")
-	   		user.EduPersonScopedAffiliation = append(user.EduPersonScopedAffiliation, "employee@byu.edu")
-	   	}
-
-	   	if contractor {
-	   		user.EduPersonAffiliation = append(user.EduPersonAffiliation, "contractor")
-	   		user.EduPersonScopedAffiliation = append(user.EduPersonScopedAffiliation, "contractor@byu.edu")
-	   	}
-
-	   	if library {
-	   		user.EduPersonAffiliation = append(user.EduPersonAffiliation, "library-walk-in")
-	   		user.EduPersonScopedAffiliation = append(user.EduPersonScopedAffiliation, "library-walk-in@byu.edu")
-	   	}
-
-	   // Return JSON response
-	   w.Header().Set("Content-Type", "application/json")
-
-	   w.WriteHeader(http.StatusOK)
-
-	   // Encode struct to JSON and write the response
-
-	   	if err := json.NewEncoder(w).Encode(user); err != nil {
-	   		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
-	   	}
-	*/
+	// Optional: log the person struct to console
+	fmt.Printf("Person: %+v\n", person)
 }
 
 func help(w http.ResponseWriter, r *http.Request) {
